d82caec5b759b57e77bc1231d28265c330fa7736
Josh Keller <JSKeller975@gmail.com>
2023-02-19 13:04:13 -0500

    remove console.log from files.js

--- helpers/files.js
+++ helpers/files.js
@@ -3,8 +3,7 @@ const fsPromises = require("fs").promises;
 function getDataFromFile(pathToFile) {
   return fsPromises
     .readFile(pathToFile, { encoding: "utf-8" })
-    .then((data) => JSON.parse(data))
-    .catch((err) => console.log(err));
+    .then((data) => JSON.parse(data));
 }
 
 module.exports = getDataFromFile;
