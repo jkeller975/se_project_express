2b8595e313fd39229645741ded90513379831334
Josh Keller <JSKeller975@gmail.com>
2023-03-08 16:22:39 -0500

    Adjust Error Messages

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ mongoose.connect("mongodb://localhost:27017/aroundb", {
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/cards", cardsRouter);
+app.use((req, res, next) => {
+  res.status(404).send({ message: "Requested resource not found" });
+  next();
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- utils/errors.js
+++ utils/errors.js
@@ -1,7 +1,5 @@
 module.exports.checkErrors = ({ res, err }) => {
-  if (err.message === "Not Found") {
-    res.status(404).send({ message: "Resource not found" });
-  } else if (err.name === "ValidationError" || err.name === "CastError") {
+  if (err.name === "ValidationError" || err.name === "CastError") {
     res.status(400).send({ message: "Data is invalid" });
   } else {
     res.status(500).send({ message: "An error has occurred on the server." });
